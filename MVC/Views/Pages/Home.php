7c453d92a45c8b941d94cf22701200fbdd70e570
TheHiep0902 <caothehiep2003@gmail.com>
2023-09-30 14:55:08 +0700

    Update Home.php

--- MVC/Views/Pages/Home.php
+++ MVC/Views/Pages/Home.php
@@ -12,8 +12,8 @@
 
 <body>
     <form action="http://localhost/BTL_QuanLyDiem/Home/Dangnhap" method="post">
-            <div class="container-fluid">
-                <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="container-fluid" style="margin-top: 170px;">
+                <div class="row d-flex justify-content-center align-items-center">
                     <div class="col-md-9 col-lg-6 col-xl-5">
                         <img src="Public/Images/logo-utt-border.png" class="img-fluid" alt="UTT image">
                     </div>
